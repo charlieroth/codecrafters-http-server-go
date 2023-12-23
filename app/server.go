6b686bd04ce43d3e7de84cb10347e5ced93d862d
Charlie Roth <charlieroth4@icloud.com>
2023-12-23 10:54:21 +0100

    respond with 200

--- app/server.go
+++ app/server.go
@@ -7,17 +7,29 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = listener.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	body := make([]byte, 1024)
+	_, err = conn.Read(body)
+	if err != nil {
+		fmt.Println("Failed to read bytes from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Failed to write bytes to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
